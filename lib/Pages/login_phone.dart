ac3fc354ca04da54ad0ccfacbe1023e89322b592
Ganesh Ban <banganesh98@gmail.com>
2022-01-23 19:00:21 -0500

    Starting Development env.

---
+++ lib/Pages/login_phone.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginByPhone extends StatelessWidget {
+  const LoginByPhone({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
